9e6fb19d53e0d98fafd3f11676f913d455822f4f
zhang <j.zhiyun@icloud.com>
2024-06-06 05:11:06 -0400

    Add settings save and load feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 MOD_DIRECTORY = 'mod_directory'
 MOD_NAME = 'mod_name'
@@ -17,24 +18,48 @@ class Options:
 
 
 class OptionsController:
+
+    # Return None if failed
     @staticmethod
-    def load(file_name: str = CONFIG_FILE_PATH) -> Options:
-        with open(file_name, 'r') as f:
-            option = json.load(f)
+    def load(file_name: str = CONFIG_FILE_PATH) -> Optional[Options]:
+        try:
+            with open(file_name, 'r') as f:
+                option = json.load(f)
+        except IOError:
+            return None
         return option
 
+    # Return False if failed
     @staticmethod
-    def save(option: Options, file_name: str = CONFIG_FILE_PATH) -> None:
-        with open(file_name, 'w') as f:
-            json.dump(option, f)
+    def save(options: Options, file_name: str = CONFIG_FILE_PATH) -> bool:
+        try:
+            with open(file_name, 'w') as f:
+                json.dump(options, f)
+        except IOError:
+            return False
+        return True
 
     @staticmethod
-    def create_empty(file_name: str = CONFIG_FILE_PATH) -> None:
-        OptionsController.save(Options(), file_name)
+    def is_invalid(options: Optional[Options]) -> bool:
+        return options is None or options.mod_directory == '' or options.mod_name == ''
 
 
 def main() -> None:
-    pass
+    # load options
+    options = OptionsController.load()
+
+    # new user
+    if OptionsController.is_invalid(options):
+        if options is None:
+            options = Options()
+        while not options.mod_directory:
+            options.mod_directory = input('请输入模拟器模组文件夹路径:')
+        while not options.mod_name:
+            options.mod_name = input('请输入模组名称:')
+        # save the options
+        OptionsController.save(options)
+    print(f'当前模组文件夹路径: {options.mod_directory}')
+    print(f'当前模组名: {options.mod_name}')
 
 
 if __name__ == '__main__':
